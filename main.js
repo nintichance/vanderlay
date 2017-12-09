759d12d0afdb6ea785495c51f825828d5d89b85f
nintichance <mintidance@gmail.com>
2017-12-08 19:13:59 -0500

    Got the basic webpage styled & functional

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    {
+    $("#green").hover(function(handlerIn, handlerOut){
+       $('#green').css('background-color', 'green')
+      })
+    $("#red").hover(function() {
+        $('#red').css('background-color', 'red')
+       })
+    $("#yellow").hover(function() {
+        $('#yellow').css('background-color', 'yellow')
+       })
+    $("#blue").hover(function() {
+        $('#blue').css('background-color', 'blue')
+       })
+    }
+})
